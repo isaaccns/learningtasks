934a6f2c1d1f799ffd4650dbbd91bab4a5b9dcb6
isaaccns <99164447+isaaccns@users.noreply.github.com>
2022-02-07 14:25:56 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Home Page</title>
+    </head>
+    <body>
+    <h1>Welcome to My Website! I am Isaac!</h1>
+    <ul>
+       <li><a href="index.php">HOME</a></i>
+       <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+    </ul>
+    </body>
+ </html>

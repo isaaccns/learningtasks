7db2ef838b64dd50f75fec3b6261d84306c9bcee
isaaccns <99164447+isaaccns@users.noreply.github.com>
2022-03-06 23:56:49 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>My Home Page</title>
-    </head>
-    <body>
-    <h1>Welcome to My Website! I am Isaac!</h1>
-    <ul>
-       <li><a href="index.php">HOME</a></li>
-       <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
-    </ul>
-    </body>
- </html>
+	<head>
+		<title>My Home Page</title>
+	</head>
+	<body>
+		<h1>Welcome to My Website! I am Isaac :D</h1>
+			<ul>
+		 		<li><a href="/index.php">HOME</a></li>
+      				<li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+	  			    <li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
+    			</ul>
+	</body>
+	</html>

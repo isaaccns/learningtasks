785ad9068dcdc101dadf7c9667d6e056b7eef055
isaaccns <99164447+isaaccns@users.noreply.github.com>
2022-03-18 07:00:25 -0700

    Add files via upload

---
+++ CLE1MT/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My Home Page</title>
+	</head>
+	<body>
+		<h1>Welcome to My Website! I am Isaac :D</h1>
+			<ul>
+		 		<li><a href="/index.php">HOME</a></li>
+      				<li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+	  			    <li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
+					<li><a href="/CLE1MT/index.html">CULMINATING LEARNING EVIDENCE</a></li>
+    			</ul>
+	</body>
+	</html>

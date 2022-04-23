43c56114a2e68886ae70ef27b891d571fc17a30c
isaaccns <99164447+isaaccns@users.noreply.github.com>
2022-04-23 14:50:38 +0800

    Add files via upload

---
+++ LT5/index.js
@@ -0,0 +1,24 @@
+function cndrotate1() {
+  document.getElementById("Candidate1").src = "button.png";
+}
+function cndstop1(){
+  document.getElementById("Candidate1").src = "button.png";
+}
+function cndclick1(){
+  document.getElementById("Candidate1").src = "bbmsara.png";
+  var audio = new Audio('bbmsara.mp3');
+  audio.play();
+}
+
+ 
+function cndrotate2() {
+  document.getElementById("Candidate2").src = "button.png";
+}
+function cndstop2(){
+  document.getElementById("Candidate2").src = "button.png";
+}
+function cndclick2(){
+  document.getElementById("Candidate2").src = "lenikiko.png";
+  var audio = new Audio('rosas.mp3');
+  audio.play();
+}

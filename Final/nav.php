0197eede67a5f4747b0b8dc9c8f57033718f482c
isaaccns <99164447+isaaccns@users.noreply.github.com>
2022-05-23 10:39:47 +0800

    Update nav.php

--- Final/nav.php
+++ Final/nav.php
@@ -1,5 +1,5 @@
 <ul>
 		<li><a onmouseenter='play()' href="index.php">HOME</a> </li>
-		<li><a onmouseenter='play()' href="profile.php">PROFILE</a> </li>
 		<li><a onmouseenter='play()' href="compworks.php">COMPILED WORKS</a> </li>
+		<li><a onmouseenter='play()' href="profile.php">PROFILE</a> </li>
 		</ul>
